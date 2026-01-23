7e431472ea41afa104926432a59d093af8c88e0f
Trung180804 <trung1808204@gmail.com>
2026-01-23 09:31:45 +0700

    Update/4/Login with google authenticator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,140 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { TwoFactorAuthDto } from './dto/twoFactorAuth.dto';
+import { Controller, Post, Body, UnauthorizedException, BadRequestException, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthRegisterDto } from './authRegister.dto';
-import { AuthForgotPasswordDTO } from './authForgotPassword.dto';
+import { AuthRegisterDto } from './dto/authRegister.dto';
+import { AuthForgotPasswordDTO } from './dto/authForgotPassword.dto';
 import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
-import { AuthLoginDto } from './authLogin.dto';
+import { AuthLoginDto } from './dto/authLogin.dto';
+import { UsersService } from '@/users/users.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { access } from 'fs';
+
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @Post('register')
-  @ApiOperation({ summary: 'Dang ky tai khoan' })
+  @ApiOperation({ summary: 'Register' })
   @ApiBody({ type: AuthRegisterDto })
   async register(@Body() registerDto: AuthRegisterDto) {
     return this.authService.register(registerDto.email, registerDto.password);
   }
 
   @Post('login')
-  @ApiOperation({ summary: 'Dang nhap he thong' })
+  @ApiOperation({ summary: 'Login' })
   async login(@Body() loginDto: AuthLoginDto) {
     return this.authService.login(loginDto.email, loginDto.password);
   }
 
+  @Post('2fa/enable')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '2FA Enable - Verify OTP and turn on 2FA' })
+  async enable2FA(
+    @Request() req, 
+    @Body('twoFactorAuthCode') code: string,
+) {
+    const user = await this.usersService.findOne(req.user.userId);
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    if (!user.twoFactorAuthSecret) {
+      throw new BadRequestException('Please setup 2FA first');
+    }
+
+    const isValid = await this.authService.isTwoFactorAuthenticationCodeValid(
+      code,
+      user,
+    );
+
+    if (!isValid) {
+      throw new BadRequestException('Invalid OTP');
+    }
+
+    user.isTwoFactorAuthEnabled = true;
+    await user.save();
+
+    return { message: '2FA enabled successfully' };
+  }
+
+  @Post('2fa/setup')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '2FA Setup' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        userId: { type: 'string' },
+      },
+      required: ['userId'],
+    },
+  })
+  async setup2FA(@Request() req) {
+    const userId = req.user.userId;
+    const setupUser = await this.usersService.findOne(userId);
+    if (!setupUser) {
+      throw new BadRequestException('User not found');
+    }
+
+    const { qrDataUrl } =
+      await this.authService.generateTwoFactorAuthenticationSecret(setupUser);
+
+    return qrDataUrl;
+  }
+
+  @Post('2fa/verify')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '2FA Verify' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        twoFactorAuthCode: { type: 'string' },
+      },
+      required: ['twoFactorAuthCode'],
+    },
+  })
+  async verify2FA(
+    @Request() req,
+    @Body('twoFactorAuthCode') code: string,
+  ) {
+
+    const userId = req.user.userId;
+    const user = await this.usersService.findOne(userId);
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    const isValid = await this.authService
+      .isTwoFactorAuthenticationCodeValid(code, user);
+
+    if (!isValid) {
+      throw new BadRequestException('Invalid OTP');
+    }
+
+    user.isTwoFactorAuthEnabled = true;
+    await user.save();
+
+    return {
+      message: '2FA enabled successfully',
+      access_token: this.authService.generateJwt(user).access_token };
+  }
+
   @Post('forgotPassword')
-  @ApiOperation({ summary: 'Gui ma OTP quen mat khau qua MailHog' })
+  @ApiOperation({ summary: 'Forgot Password' })
   async forgotPassword(@Body() forgotPasswordDto: AuthForgotPasswordDTO) {
     return this.authService.forgotPassword(forgotPasswordDto.email);
   }
-  /* code mac dinh he thong
+  /* 
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -8,6 +8,10 @@ export class User extends Document {
   @Prop({ required: true }) password: string;
 
   @Prop() resetToken: string;
+
+  @Prop({default: null}) twoFactorAuthSecret: string;
+
+  @Prop({ default: false }) isTwoFactorAuthEnabled: boolean;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
