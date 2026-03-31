321a090d64f90895fd0cdde6f869d3bee320223c
Trung180804 <trung1808204@gmail.com>
2026-03-31 11:07:22 +0700

    T11/Update.8/ Fix ACL from S3 upload

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -30,7 +30,7 @@ export class AwsService {
         Key: uniqueFileName,
         Body: file.buffer,
         ContentType: file.mimetype,
-        ACL: 'public-read',
+        // ACL: 'public-read',
       });
 
       await this.s3Client.send(command);
